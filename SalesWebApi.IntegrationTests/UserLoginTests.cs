3110cfb64d959c5a4e867939cd514951d6bf9d62
janos-laszlo <janos-laszlo@outlook.com>
2026-02-04 21:23:43 +0200

    Test token validation

--- SalesWebApi.IntegrationTests/UserLoginTests.cs
+++ SalesWebApi.IntegrationTests/UserLoginTests.cs
@@ -1,3 +1,7 @@
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 using UserIdentity.Commands;
 
 namespace SalesWebApi.IntegrationTests;
@@ -47,5 +51,74 @@ public sealed class UserLoginTests(UserIdentityFixture fixture)
         var result = await fixture.Client.PostAsJsonAsync(
             "login", credentials with { Password = "WrongPassword2"});
         Assert.False(result.IsSuccessStatusCode);
+    }
+
+    [Fact]
+    public async Task Fails_when_JWT_is_tampered_with()
+    {
+        // Arrange
+        UserCredentialsDto credentials = new(UserUtils.NextEmail, "Password1");
+        var tokenResponse = await RegisterAndLoginUser(credentials);
+
+        // Act
+        var statusCode = await GetHealthCheckWithToken(tokenResponse.AccessToken);
+        Assert.Equal(System.Net.HttpStatusCode.OK, statusCode);
+        
+        // Tamper with the token
+        var token = CreateAccessedTokenWithIncorrectSignatureKey("1", credentials.Email);
+        // Try to use the tampered token for a protected endpoint
+        var protectedResult = await GetHealthCheckWithToken(token);
+        Assert.Equal(System.Net.HttpStatusCode.Unauthorized, protectedResult);
+    }
+    
+    private async Task<TokenResponseDto> RegisterAndLoginUser(UserCredentialsDto credentials)
+    {
+        var registrationResult = await fixture.Client.PostAsJsonAsync(
+            "register", credentials);
+        Assert.True(registrationResult.IsSuccessStatusCode);
+
+        var loginResult = await fixture.Client.PostAsJsonAsync(
+            "login", credentials);
+        Assert.True(loginResult.IsSuccessStatusCode);
+
+        var body = await loginResult.Content.ReadFromJsonAsync<TokenResponseDto>();
+        Assert.NotNull(body);
+        return body;
+    }
+    
+    private static string CreateAccessedTokenWithIncorrectSignatureKey(string userId, string email)
+    {
+        var data = Encoding.UTF8.GetBytes("some incorrect very_secret key h4iot-");
+        var securityKey = new SymmetricSecurityKey(data);
+
+        var claims = new Dictionary<string, object>
+        {
+            [JwtRegisteredClaimNames.Sub] = userId,
+            [JwtRegisteredClaimNames.Email] = email
+        };
+        var descriptor = new SecurityTokenDescriptor
+        {
+            Issuer = "http://localhost:1234",
+            Audience = "http://localhost:1234",
+            Claims = claims,
+            IssuedAt = DateTime.UtcNow,
+            NotBefore = DateTime.UtcNow,
+            Expires = DateTime.UtcNow.AddMinutes(120),
+            SigningCredentials = new SigningCredentials(
+                securityKey, SecurityAlgorithms.HmacSha256)
+        };
+
+        var jwtHandler = new JsonWebTokenHandler();
+        return jwtHandler.CreateToken(descriptor);
+    }
+
+    private async Task<System.Net.HttpStatusCode> GetHealthCheckWithToken(string accessToken)
+    {
+        using var requestMessage = new HttpRequestMessage(HttpMethod.Get, "health");
+        requestMessage.Headers.Authorization =
+            new AuthenticationHeaderValue("Bearer", accessToken);
+
+        using var responseMessage = await fixture.Client.SendAsync(requestMessage);
+        return responseMessage.StatusCode;
     }
 }
--- UserIdentity/Commands/JwtService.cs
+++ UserIdentity/Commands/JwtService.cs
@@ -39,7 +39,7 @@ internal sealed class JwtService(
             Claims = claims,
             IssuedAt = DateTime.UtcNow,
             NotBefore = DateTime.UtcNow,
-            Expires = DateTime.UtcNow.AddMinutes(120),
+            Expires = DateTime.UtcNow.AddMinutes(5),
             SigningCredentials = new SigningCredentials(
                 securityKey, SecurityAlgorithms.HmacSha256)
         };

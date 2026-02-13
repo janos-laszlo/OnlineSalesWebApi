39da73d2eaca05be983b498fa8b9ac294f44b56b
janos-laszlo <janos-laszlo@outlook.com>
2026-02-13 21:07:15 +0200

    Refactor to functional expressions

--- SalesWebApi/Endpoints/IdentityEndpoints.cs
+++ SalesWebApi/Endpoints/IdentityEndpoints.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using CSharpFunctionalExtensions;
 using Microsoft.AspNetCore.Mvc;
 using UserIdentity.Commands;
 using UserIdentity.Extensions;
@@ -23,99 +24,98 @@ public static class IdentityEndpoints
 
         accountGroup.MapPost(
             Register,
-            [RequestSizeLimit(1024)] async (UserCredentialsDto userRegistrationDto,
-            IRegisterUserCommand registerUserCommand,
-            CancellationToken cancellationToken) =>
-            {
-                var registerUserCommandResult = await registerUserCommand.Execute(
-                    userRegistrationDto, cancellationToken);
-                return registerUserCommandResult.IsSuccess
-                    ? Results.Ok()
-                    : Results.Problem(
-                        title: "User registration failed",
-                        detail: registerUserCommandResult.Error,
-                        statusCode: 400);
-            });
+            [RequestSizeLimit(1024)] async (
+                UserCredentialsDto userRegistrationDto,
+                IRegisterUserCommand registerUserCommand,
+                CancellationToken cancellationToken)
+            =>
+                await registerUserCommand
+                    .Execute(userRegistrationDto, cancellationToken)
+                    .Finally(result => result.IsSuccess
+                        ? Results.Ok()
+                        : Results.Problem(
+                            title: "User registration failed",
+                            detail: result.Error,
+                            statusCode: StatusCodes.Status400BadRequest)));
 
         accountGroup.MapPost(
             Login,
             async (UserCredentialsDto userLoginDto,
-            ILoginUserCommand loginUserCommand,
-            CancellationToken cancellationToken) =>
-        {
-            var loginResult = await loginUserCommand.Execute(userLoginDto, cancellationToken);
-            return loginResult.IsSuccess
-                ? Results.Ok(loginResult.Value)
-                    : Results.Problem(
-                        title: "User login failed",
-                        detail: loginResult.Error,
-                        statusCode: 400);
-        });
+                ILoginUserCommand loginUserCommand,
+                CancellationToken cancellationToken)
+            =>
+                await loginUserCommand
+                    .Execute(userLoginDto, cancellationToken)
+                    .Finally(loginResult => loginResult.IsSuccess
+                        ? Results.Ok(loginResult.Value)
+                        : Results.Problem(
+                            title: "User login failed",
+                            detail: loginResult.Error,
+                            statusCode: StatusCodes.Status400BadRequest)));
 
         accountGroup.MapPost(
             RefreshToken,
             async (RefreshTokenRequestDto refreshTokenDto,
-            IRefreshTokenCommand refreshTokenCommand,
-            CancellationToken cancellationToken) =>
-            {
-                var refreshToken = await refreshTokenCommand.Execute(
-                    refreshTokenDto.RefreshToken, cancellationToken);
-                return refreshToken.IsSuccess
-                    ? Results.Ok(refreshToken.Value)
-                    : Results.Problem(
-                        title: "Refreshing token failed",
-                        detail: refreshToken.Error,
-                        statusCode: 400);
-            });
+                IRefreshTokenCommand refreshTokenCommand,
+                CancellationToken cancellationToken)
+            =>
+                await refreshTokenCommand
+                    .Execute(refreshTokenDto.RefreshToken, cancellationToken)
+                    .Finally(refreshToken => refreshToken.IsSuccess
+                        ? Results.Ok(refreshToken.Value)
+                        : Results.Problem(
+                            title: "Refreshing token failed",
+                            detail: refreshToken.Error,
+                            statusCode: StatusCodes.Status400BadRequest)));
 
         accountGroup.MapGet(
             ConfirmEmail,
             async (string token,
-            IConfirmEmailCommand confirmEmailCommand,
-            CancellationToken cancellationToken) =>
-            {
-                var confirmEmailCommandResult = await confirmEmailCommand.Execute(token, cancellationToken);
-                return confirmEmailCommandResult.IsSuccess
-                    ? Results.Ok()
-                    : Results.Problem(
-                        title: "Email confirmation failed",
-                        detail: confirmEmailCommandResult.Error,
-                        statusCode: 400);
-            });
+                IConfirmEmailCommand confirmEmailCommand,
+                CancellationToken cancellationToken)
+            =>
+                await confirmEmailCommand
+                    .Execute(token, cancellationToken)
+                    .Finally(confirmEmailCommandResult => confirmEmailCommandResult.IsSuccess
+                        ? Results.Ok()
+                        : Results.Problem(
+                            title: "Email confirmation failed",
+                            detail: confirmEmailCommandResult.Error,
+                            statusCode: StatusCodes.Status400BadRequest)));
 
-        accountGroup.MapGet(
-            Profile,
-            async (IGetUserProfileCommand getUserInfoCommand,
-            ClaimsPrincipal httpContextUser,
-            CancellationToken cancellationToken) =>
-            {
-                var result = await getUserInfoCommand.Execute(
-                    httpContextUser.UserId, cancellationToken);
-                return result.IsSuccess
-                    ? Results.Ok(result.Value)
-                    : Results.Problem(
-                        title: "Getting user info failed",
-                        detail: result.Error,
-                        statusCode: 404);
-            })
+        accountGroup
+            .MapGet(
+                Profile,
+                async (IGetUserProfileCommand getUserInfoCommand,
+                    ClaimsPrincipal httpContextUser,
+                    CancellationToken cancellationToken)
+                =>
+                    await getUserInfoCommand
+                        .Execute(httpContextUser.UserId, cancellationToken)
+                        .Finally(result => result.IsSuccess
+                            ? Results.Ok(result.Value)
+                            : Results.Problem(
+                                title: "Getting user info failed",
+                                detail: result.Error,
+                                statusCode: StatusCodes.Status404NotFound)))
             .RequireAuthorization();
 
-        accountGroup.MapPost(
-            Profile,
-            async (UserProfileRequestDto userProfile,
-            IUpdateUserProfileCommand setUserProfileCommand,
-            ClaimsPrincipal httpContextUser,
-            CancellationToken cancellationToken) =>
-            {
-                var result = await setUserProfileCommand.Execute(
-                    httpContextUser.UserId, userProfile, cancellationToken);
-                return result.IsSuccess
-                    ? Results.Ok()
-                    : Results.Problem(
-                        title: "Profile update failed",
-                        detail: result.Error,
-                        statusCode: 400);
-            })
+        accountGroup
+            .MapPost(
+                Profile,
+                async (UserProfileRequestDto userProfile,
+                    IUpdateUserProfileCommand setUserProfileCommand,
+                    ClaimsPrincipal httpContextUser,
+                    CancellationToken cancellationToken)
+                =>
+                    await setUserProfileCommand
+                        .Execute(httpContextUser.UserId, userProfile, cancellationToken)
+                        .Finally(result => result.IsSuccess
+                            ? Results.Ok()
+                            : Results.Problem(
+                                title: "Profile update failed",
+                                detail: result.Error,
+                                statusCode: StatusCodes.Status400BadRequest)))
             .RequireAuthorization();
 
         accountGroup.MapGet(Health, () => Results.Ok("Healthy"))
